5cb0707d6728533ecf699268a0f65316fe73ff54
denys-dharma <denys@dharma.nl>
2023-08-14 12:02:38 +0300

    Add restart function

--- bin/functions/docker.sh
+++ bin/functions/docker.sh
@@ -8,6 +8,12 @@ function dockerStart {
     dockerContainersLog
 }
 
+# Restart docker compose
+function dockerRestart {
+  docker-compose -f "$initLocation/docker-compose.yml" restart &&
+    dockerContainersLog
+}
+
 # Rebuild and start docker compose
 function dockerRebuild() {
   docker builder prune && docker-compose -f "$initLocation/docker-compose.yml" up -d --build &&
